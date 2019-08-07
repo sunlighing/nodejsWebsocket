7597b32b24ac43449ae61a454457536f43798f07
Cloud <dzclong@outlook.com>
2019-08-07 15:48:47 +0800

    漏了个文件

---
+++ msgQueue.js
@@ -0,0 +1,61 @@
+var msgServer = require("./msgServer");
+
+function msgQueue() {
+    //这里是属性和方法
+    let msgserver = null
+    this.init=function(obj){
+        msgserver = obj;
+        //初始化成功
+        console.log(msgserver);
+    }
+
+    let data={
+        uid: "",
+        name: "",
+        act: "",
+        public: {
+
+        }
+    }
+
+    let user={
+        name: "",
+        act: 0,
+        msg: "",
+    } 
+
+    let items = [];
+
+    this.enqueue = function (data) {
+        
+        items.push(data);
+        this.publicSend("dd");
+    };
+
+    this.publicSend = function(msg){
+
+        msgserver.publicData(data);
+    }
+
+    this.dequeue = function () {
+        return items.shift();
+    };
+
+    this.front = function () {
+        return items[0];
+    };
+
+    this.isEmpty = function () {
+        return items.length == 0;
+    };
+
+    this.size = function () {
+        return items.length;
+    };
+
+    this.print = function () {
+        console.log(items.toString());
+    };
+}
+
+module.exports = msgQueue;
